c51da92d90e9d7f208f01d9c1772738b83e002a0
User <bashkatovr@mail.ru>
2022-11-22 20:16:15 +0300

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        // Задание 1
+        //Задание 1
         System.out.println("Задание 1");
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <=10 ; i++) {
             System.out.println(i);
         }
         // Задание 2
         System.out.println("Задание 2");
-        for (int i = 10; i <= 1; i--) {
+        for (int i = 10; i >=0 ; i--) {
             System.out.println(i);
+
         }
+
     }
 
 }
