8ea8ea26353845251918f0f313bca0c406183729
User <bashkatovr@mail.ru>
2022-11-23 20:42:56 +0300

    commit 5

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,11 @@ public class Main {
         System.out.println("Задание 4");
         for (int i = 10; i >=-10 ; i--) {
             System.out.println(i);
-
+        }
+        // Задание 5
+        System.out.println("Задание 5");
+        for (int i = 1904; i <= 2096; i=i+4) {
+            System.out.println(i);
         }
 
     }
